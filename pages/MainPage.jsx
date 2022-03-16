b2392bff68ce2a3c0cff9750d67e6c632ffec1ee
HyungjunJeon <maru105kr@gmail.com>
2022-03-16 23:37:36 +0900

    Create MainPage component

---
+++ pages/MainPage.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+import { ScrollView, Text, StyleSheet, Image, TouchableOpacity } from "react-native"
+
+export default function MainPage() {
+  return (
+    <ScrollView>
+      <Text>This is Main Page.</Text>
+    </ScrollView>
+  )
+}
+
+const styles = StyleSheet.create({})
